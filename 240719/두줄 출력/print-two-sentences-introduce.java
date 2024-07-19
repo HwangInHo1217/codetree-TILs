8478ae5bda53147d275a5b39f8f397bfcfc7a5ed
황인호 <76863802+HwangInHo1217@users.noreply.github.com>
2024-07-19 17:40:36 +0900

    Title: 두줄 출력, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 240719/두줄 출력/print-two-sentences-introduce.java
+++ 240719/두줄 출력/print-two-sentences-introduce.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
           System.out.println("Hello students!\n" +
-                "Welcome to LeebrosCode!!");
+                "Welcome to LeebrosCode!");
     }
 }
