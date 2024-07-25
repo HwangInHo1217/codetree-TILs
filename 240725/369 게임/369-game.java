8f4b310a3da72b1a3590d922e7bb574a9b7c4911
황인호 <76863802+HwangInHo1217@users.noreply.github.com>
2024-07-25 18:24:05 +0900

    Title: 369 게임, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240725/369 게임/369-game.java
+++ 240725/369 게임/369-game.java
@@ -7,6 +7,10 @@ public class Main {
         for(int i = 1; i <= n; i++){
            if(i > 29 && i < 40)
                System.out.print(0 + " ");
+           else if(i > 59 && i < 70)
+               System.out.print(0 + " ");
+           else if(i > 89 && i < 100)
+               System.out.print(0 + " ");
            else if (i % 10 == 0)
                System.out.print(i + " ");
            else if (i % 3 == 0 || (i % 10) % 3 == 0 || (i % 10) % 6 == 0 || (i % 10) % 9 == 0 )
