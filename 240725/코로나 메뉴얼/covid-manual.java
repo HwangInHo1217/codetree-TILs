9601624a7b36d1f8caeb8231734ab602b04003ca
황인호 <76863802+HwangInHo1217@users.noreply.github.com>
2024-07-25 17:06:49 +0900

    Title: 코로나 메뉴얼, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240725/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int Temperature[] = new int[3];
+        char State[] = new char[3];     
+        int cnt = 0;
+
+        for(int i = 0; i < Temperature.length; i++){
+            State[i] = sc.next().charAt(0);
+            Temperature[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < Temperature.length; i ++){
+            if(Temperature[i] > 37 && State[i] == 'Y')
+                cnt++;
+        }
+        if(cnt > 1)
+            System.out.print("E");
+        else
+            System.out.print("N");
+    }
+}
